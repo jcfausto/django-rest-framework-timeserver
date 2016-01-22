69f3356ac6fd0fd30c071e7ab4ae1389f6bb2279
Julio Cesar Fausto <jcfausto@gmail.com>
2016-01-22 13:15:43 +0000

    Added the TimeView

--- timeserver/views.py
+++ timeserver/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIViews
+from .mixins import JSONResponseMixin
+from datetime import datetime
+from pytz import timezone as pytz_timezone
+import pytz
 
-# Create your views here.
+class TimeView(APIView):
+
+	def get(self, request):
+		"""
+		Takes a timezone (or UTC by default) and returns the current time 
+		"""
+		timezone = request.GET.get('timezone')
+
+		#Standard check and defaults to UTC in case of missing or incorrect timezone.
+		try:
+			if timezone:
+				tz = pytz_timezone(timezone)
+			else:
+				tz = pytz.utc
+		except:
+			tz = pytz.utc
+
+		time = tz.localize(datetime.now())
+
+		#give more that needed information about the time, because we can. 
+		data = {
+			'time' : time.isoformat(),
+			'year' : time.year,
+			'month' : time.month,
+			'day' : time.day,
+			'hour' : time.hour,
+			'minute' : time.minute, 
+			'second' : time.second, 
+			'microsecond' : time.microsecond, 
+			'timezone' : time.timezone() if time.tzinfo else '', 
+		}
+
+		# returns the data formatted in JSON and sets up response
+		return JSONResponseMixin(data)
